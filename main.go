94f98deca86724a4ac9a69c3fe48b14aa8b76ea3
flisnes <haakonflisnes@gmail.com>
2024-05-23 14:13:31 +0200

    New window with some text

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 func main() {
 	app := tview.NewApplication()
 
-	list := tview.NewList().
-		AddItem("Thing 1", "The first thing", 'a', nil).
+	list := tview.NewList()
+
+	list.
+		AddItem("Thing 1", "The first thing", 'a', func() {
+			showTextWindow(app, list, "This text will be displayed in the new window.")
+		}).
 		AddItem("Thing 2", "The second thing", 'b', nil).
 		AddItem("Quit", "Press to exit", 'q', func() {
 			app.Stop()
@@ -17,4 +21,17 @@ func main() {
 	if err := app.SetRoot(list, true).Run(); err != nil {
 		panic(err)
 	}
+}
+
+func showTextWindow(app *tview.Application, list *tview.List, text string) {
+	modal := tview.NewModal().
+		SetText(text).
+		AddButtons([]string{"OK"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "OK" {
+				app.SetRoot(list, true) // Return to the list
+			}
+		})
+
+	app.SetRoot(modal, true)
 }
